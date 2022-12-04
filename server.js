e172ee1124e989c326b7343990cc7e7f4aa8a014
subham5230 <subhammohanty5230@gmail.com>
2022-12-04 15:21:41 +0530

    cors-fix

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ mongoose
 
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
-app.use(cors());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", (req,res) => {
     res.json({message: "Scheduler Service."});
